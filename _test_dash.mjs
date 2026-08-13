46ef630c619c7b12c79d6a5ceba4b8b71d4c7620
Elias <fernandezllanoselias@gmail.com>
2026-08-12 23:57:31 -0500

    Elimina archivo de prueba huérfano no debio subirse

--- _test_dash.mjs
+++
@@ -1,9 +0,0 @@
-import { obtenerDashboardAdmin } from './src/application/use-cases/obtenerDashboardAdmin.js';
-try {
-  const d = await obtenerDashboardAdmin();
-  console.log('metricas:', JSON.stringify(d.metricas));
-  console.log('usuarios devueltos:', d.usuarios.length);
-  d.usuarios.forEach(u => console.log('  - id=' + u.id + ' | ' + u.name + ' | tel=' + JSON.stringify(u.phone) + ' | rol=' + u.role));
-} catch (e) {
-  console.error('FAIL ->', e.message);
-}
